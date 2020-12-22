478d0929d853567d75420330386e129a43ef11b0
namroc89 <44593850+namroc89@users.noreply.github.com>
2020-12-22 13:28:31 -0600

    part 1 of biztime

---
+++ express-biztime/routes/companies.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const router = new express.Router();
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+// Get / returns all companies, like {companies: [{company}]}
+router.get("/", async (req, res, next) => {
+  const result = await db.query(`SELECT * FROM companies`);
+  return res.json({ companies: result.rows });
+});
+
+//  Get /[code] Return obj of company: {company: {code, name, description}}
+
+router.get("/:code", async (req, res, next) => {
+  try {
+    const companyResult = await db.query(
+      `
+    SELECT * 
+    FROM companies 
+    WHERE code = $1`,
+      [req.params.code]
+    );
+    const invoiceResult = await db.query(
+      `
+    SELECT id 
+    FROM invoices 
+    WHERE comp_code = $1`,
+      [req.params.code]
+    );
+
+    if (companyResult.rows.length === 0) {
+      throw new ExpressError(`No company with code ${req.params.code}`, 404);
+    }
+    const company = companyResult.rows[0];
+    const invoices = invoiceResult.rows;
+
+    company.invoices = invoices.map((inv) => inv.id);
+
+    return res.json({ company: company });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+// Post / create new company, Returns obj of new company: {company: {code, name, description}}
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { code, name, description } = req.body;
+    const result = await db.query(
+      `
+    INSERT INTO companies (code, name, description)
+    VALUES ($1, $2, $3)
+    RETURNING code, name, description`,
+      [code, name, description]
+    );
+    return res.status(201).json({ company: result.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+// PUT /:code Edits company and Returns update company object: {company: {code, name, description}}
+
+router.put("/:code", async (req, res, next) => {
+  try {
+    if ("code" in req.body) {
+      throw new ExpressError("Not allowed", 400);
+    }
+    const { name, description } = req.body;
+    const result = await db.query(
+      `UPDATE companies 
+    SET name =$2, description = $3
+    WHERE code = $1
+    RETURNING code, name, description
+    `,
+      [req.params.code, name, description]
+    );
+    if (result.rows.length === 0) {
+      throw new ExpressError(`No company with code ${req.params.code}`, 404);
+    }
+    return res.json({ company: result.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+// Delete /:code, Deletes company returns Returns {status: "deleted"}
+
+router.delete("/:code", async (req, res, next) => {
+  try {
+    const result = await db.query(
+      `DELETE FROM companies
+        WHERE code = $1`,
+      [req.params.code]
+    );
+
+    return res.json({ message: "Deleted" });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+module.exports = router;
---
+++ express-biztime/routes/invoices.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const router = new express.Router();
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+// Get / returns all companies, like {companies: [{company}]}
+router.get("/", async (req, res, next) => {
+  const result = await db.query(`SELECT id, comp_code FROM invoices`);
+  return res.json({ invoices: result.rows });
+});
+
+// Get /:id returns company with given ID and corresponding company Returns {invoice: {id, amt, paid, add_date, paid_date, company: {code, name, description}}}
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const result = await db.query(
+      `
+    SELECT i.id,
+     i.comp_code,
+     i.amt,
+     i.paid,
+     i.add_date,
+     i.paid_date,
+     c.name,
+     c.description 
+     FROM invoices As i 
+     JOIN companies AS c ON(i.comp_code = c.code)
+    WHERE id = $1`,
+      [req.params.id]
+    );
+    if (result.rows.length === 0) {
+      throw new ExpressError(`No invoice with ID ${req.params.id}`, 404);
+    }
+    const data = result.rows[0];
+    const invoice = {
+      id: data.id,
+      amt: data.amt,
+      paid: data.paid,
+      add_date: data.add_date,
+      paid_date: data.paid_date,
+      company: {
+        code: data.comp_code,
+        name: data.name,
+        description: data.description,
+      },
+    };
+
+    return res.json({ invoice: invoice });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+//Post / creates new invoice when passes comp_code and amt, Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { comp_code, amt } = req.body;
+    const result = await db.query(
+      `
+      INSERT INTO invoices (comp_code, amt)
+      VALUES ($1, $2)
+      RETURNING id,comp_code,amt,paid,add_date,paid_date`,
+      [comp_code, amt]
+    );
+    return res.status(201).json({ invoice: result.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+//  PUT /:id Edits Invoice amount and Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    if ("id" in req.body) {
+      throw new ExpressError("Not allowed", 400);
+    }
+    const { amt } = req.body;
+    const result = await db.query(
+      `UPDATE invoices 
+      SET amt =$2
+      WHERE id = $1
+      RETURNING id,comp_code,amt,paid,add_date,paid_date
+      `,
+      [req.params.id, amt]
+    );
+    if (result.rows.length === 0) {
+      throw new ExpressError(`No invoice with ID ${req.params.code}`, 404);
+    }
+    return res.json({ invoice: result.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+// Delete /:id, Deletes invoice returns Returns {status: "deleted"}
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const result = await db.query(
+      `DELETE FROM invoices
+          WHERE id = $1`,
+      [req.params.id]
+    );
+
+    return res.json({ message: "Deleted" });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+module.exports = router;
