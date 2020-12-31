0e921cfd2e6f024f076cafac87c8972cfa960474
namroc89 <44593850+namroc89@users.noreply.github.com>
2020-12-31 08:32:06 -0600

    messagely basic functionality

--- express-messagely/models/user.js
+++ express-messagely/models/user.js
@@ -1,29 +1,83 @@
 /** User class for message.ly */
 
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const ExpressError = require("../expressError");
 
-
+const { BCRYPT_WORK_FACTOR } = require("../config");
 /** User of the site. */
 
 class User {
-
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {
+    let hashedPass = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `
+        INSERT INTO users (
+          username, 
+          password, 
+          first_name, 
+          last_name, 
+          phone, 
+          join_at, 
+          last_login_at)
+        VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) 
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPass, first_name, last_name, phone]
+    );
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    if (!username || !password) {
+      throw new ExpressError("Username and Password required.", 400);
+    }
+    const result = await db.query(
+      `SELECT username, password 
+      FROM users 
+      WHERE username = $1`,
+      [username]
+    );
+    const user = result.rows[0];
+    if (user) {
+      if (await bcrypt.compare(password, user.password)) {
+        return true;
+      }
+      return false;
+    }
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    await db.query(
+      `
+    UPDATE users 
+    SET last_login_at = current_timestamp
+    WHERE username = $1
+    RETURNING username,first_name,last_name,phone`,
+      [username]
+    );
+    return;
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query(`
+    SELECT username, first_name, last_name, phone
+    FROM users`);
+    if (!result) {
+      throw new ExpressError("Something went worng", 500);
+    }
+    return result.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +88,20 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(
+      `
+    SELECT username,first_name,last_name,phone,join_at,last_login_at
+    FROM users 
+    WHERE username = $1 
+    `,
+      [username]
+    );
+    if (!result) {
+      throw new ExpressError("Couldn't find user", 401);
+    }
+    return result.rows[0];
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +111,38 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const result = await db.query(
+      `
+    SELECT m.id,
+    m.body,
+    m.to_username,
+    m.sent_at,
+    m.read_at,
+    u.username,
+    u.first_name,
+    u.last_name,
+    u.phone 
+    FROM messages as m
+    JOIN users AS u ON m.to_username = u.username
+    WHERE from_username = $1`,
+      [username]
+    );
+
+    let resultsList = result.rows.map((m) => ({
+      id: m.id,
+      to_user: {
+        username: m.to_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at,
+    }));
+    return resultsList;
+  }
 
   /** Return messages to this user.
    *
@@ -54,8 +152,37 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {
+    const result = await db.query(
+      `
+    SELECT m.id,
+    m.body,
+    m.from_username,
+    m.sent_at,
+    m.read_at,
+    u.username,
+    u.first_name,
+    u.last_name,
+    u.phone 
+    FROM messages as m
+    JOIN users AS u ON m.from_username = u.username
+    WHERE to_username = $1`,
+      [username]
+    );
+    let resultsList = result.rows.map((m) => ({
+      id: m.id,
+      from_user: {
+        username: m.from_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at,
+    }));
+    return resultsList;
+  }
 }
-
 
 module.exports = User;
--- express-messagely/routes/messages.js
+++ express-messagely/routes/messages.js
@@ -1,3 +1,11 @@
+const Router = require("express").Router;
+const User = require("../models/user");
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const ExpressError = require("../expressError");
+
+const router = new Router();
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,8 +18,20 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
-
-
+router.get("/:id", ensureLoggedIn, async (req, res, next) => {
+  try {
+    let m = await Message.get(req.params.id);
+    if (
+      m.from_user.username !== req.user.username &&
+      m.to_user.username !== req.user.username
+    ) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+    return res.json({ m });
+  } catch (e) {
+    return next(e);
+  }
+});
 /** POST / - post message.
  *
  * {to_username, body} =>
@@ -19,7 +39,15 @@
  *
  **/
 
-
+router.post("/", ensureLoggedIn, async (req, res, next) => {
+  try {
+    let { to_username, body } = req.body;
+    let m = await Message.create(req.user.username, to_username, body);
+    return res.json({ m });
+  } catch (e) {
+    return next(e);
+  }
+});
 /** POST/:id/read - mark message as read:
  *
  *  => {message: {id, read_at}}
@@ -27,4 +55,17 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+router.post("/:id/read", ensureLoggedIn, async (req, res, next) => {
+  try {
+    let m = await Message.get(req.params.id);
+    if (req.user.username !== m.to_user.username) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+    let result = await Message.markRead(req.params.id);
+    return res.json({ result });
+  } catch (e) {
+    return next(e);
+  }
+});
 
+module.exports = router;
